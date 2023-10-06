2cbaeb8f5b4ae4058d3d4a8503f4eb3eb3e31096
Asiaviscardi <asia.viscardii@gmail.com>
2023-10-06 13:32:21 +0200

    inserimento struttura api

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const {crateApp} = Vue;
+const {createApp} = Vue;
 createApp({
     data(){
         return{
-            
+
+        }
+    },
+
+    methods:{
+
+        getApi(){
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    
+            .then((risposta)=>{
+
+                console.log(risposta)
+    
+            })
+    
+            .catch((errore)=>{
+                console.log(errore)
+            })
         }
     }
 
